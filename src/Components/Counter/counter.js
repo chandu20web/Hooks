460f95869a04e19c1fa84d6b9466c32e223e49e5
chandu20web <chandu20.web@gmail.com>
2023-07-21 18:07:03 +0530

    dev1 redux changes

--- src/Components/Counter/counter.js
+++ src/Components/Counter/counter.js
@@ -1,7 +1,55 @@
 import React from "react";
 
-const Counter = () => {
-  return <div>Counter</div>;
+import { connect } from "react-redux";
+
+const Counter = (props) => {
+  return (
+    <>
+      <div>
+        <button type="button" onClick={() => props.decrement()}>
+          -
+        </button>
+        <span>{props.count}</span>
+        <button type="button" onClick={() => props.increment()}>
+          +
+        </button>
+      </div>
+      <div>
+        <button type="button" onClick={() => props.reset()}>
+          Reset
+        </button>
+      </div>
+    </>
+  );
 };
 
-export default Counter;
+const mapStateToProps = (state) => {
+  return {
+    count: state.count,
+  };
+};
+
+const mapDispathToProps = (dispath) => {
+  return {
+    increment: () =>
+      dispath({
+        type: "INCREMENT",
+        payload: 2,
+      }),
+
+    decrement: () =>
+      dispath({
+        type: "DECREMENT",
+        payload: 1,
+      }),
+
+    reset: () => {
+      dispath({
+        type: "RESET",
+        payload: 0,
+      });
+    },
+  };
+};
+
+export default connect(mapStateToProps, mapDispathToProps)(Counter);
